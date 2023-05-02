5c2ac4abae5b40a401d8f0473fa6e60c9fb04636
Diego Pinto <diego.pinto357@gmail.com>
2023-05-02 00:40:33 -0300

    Remove default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { useEffect, useCallback, useState } from 'react';
 import { NodeJS } from 'capacitor-nodejs';
 import core from './core';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
   const [isNodeReady, setIsNodeReady] = useState<boolean>(false);
   const [items, setItems] = useState();
 
@@ -28,27 +24,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <br />
       {!isNodeReady ? 'Awaiting nodejs' : 'Node ready'}
       <br />
       <button onClick={onButtonClick}>Parse NF</button>
@@ -56,6 +31,6 @@ function App() {
       <code>{JSON.stringify(items, null, 2)}</code>
     </>
   );
-}
+};
 
 export default App;
