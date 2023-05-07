79f0a98e7689a73d004b4f9570a97b0f551bc063
Diego Pinto <diego.pinto357@gmail.com>
2023-05-07 13:46:33 -0300

    Integrate qr code reader with css baseline from nextui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useCallback, useState } from 'react';
-// import { NextUIProvider, Container, Button } from '@nextui-org/react';
+import { NextUIProvider, Container, Button } from '@nextui-org/react';
 import { NodeJS } from 'capacitor-nodejs';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import QrCodeOverlay from './QrCodeOverlay';
 import core from './core';
 
 const App = () => {
   const [isNodeReady, setIsNodeReady] = useState<boolean>(false);
   const [items, setItems] = useState<string>();
-  // const [displayUI, setDisplayUI] = useState<boolean>(true);
+  const [renderQrCode, setRenderQrCode] = useState<boolean>(false);
 
   useEffect(() => {
     NodeJS.whenReady().then(() => {
@@ -17,19 +18,29 @@ const App = () => {
     // return () => NodeJS.removeAllListeners();
   }, []);
 
-  const onButtonClick = useCallback(async () => {
+  const startQrReader = useCallback(async () => {
     await BarcodeScanner.checkPermission({ force: true });
-
+    document.body.style.position = 'unset';
+    document.body.style.backgroundColor = 'transparent';
     BarcodeScanner.hideBackground();
-    // setDisplayUI(false);
-
-    const result = await BarcodeScanner.startScan({
+    setRenderQrCode(true);
+    return await BarcodeScanner.startScan({
       cameraDirection: 'back',
       targetedFormats: ['QR_CODE'],
     });
+  }, []);
 
+  const stopQrReader = useCallback(() => {
+    document.body.style.position = 'relative';
+    document.body.style.backgroundColor = 'var(--nextui-colors-background)';
     BarcodeScanner.showBackground();
-    // setDisplayUI(true);
+    setRenderQrCode(false);
+    BarcodeScanner.stopScan();
+  }, []);
+
+  const onButtonClick = useCallback(async () => {
+    const result = await startQrReader();
+    stopQrReader();
 
     if (result.hasContent) {
       const qrCodeData = result.content.replace('"', '');
@@ -40,23 +51,22 @@ const App = () => {
       const data = await core.getNfData(key);
       setItems(JSON.stringify(data, null, 2));
     }
-  }, []);
-
-  // const display = displayUI ? 'block' : 'none';
+  }, [startQrReader, stopQrReader]);
 
   return (
-    <>
-      {/* <NextUIProvider>
-        <Container sm style={{ display }}> */}
-      {!isNodeReady ? 'Awaiting nodejs' : 'Node ready'}
-      <br />
-      {/* <Button onPress={onButtonClick}>Parse NF</Button> */}
-      <button onClick={onButtonClick}>Parse NF</button>
-      <br />
-      <code>{JSON.stringify(items, null, 2)}</code>
-      {/* </Container>
-      </NextUIProvider> */}
-    </>
+    <NextUIProvider>
+      {renderQrCode ? (
+        <QrCodeOverlay onCloseButtonClick={stopQrReader} />
+      ) : (
+        <Container sm>
+          {!isNodeReady ? 'Awaiting nodejs' : 'Node ready'}
+          <br />
+          <Button onPress={onButtonClick}>Parse NF</Button>
+          <br />
+          <code>{JSON.stringify(items, null, 2)}</code>
+        </Container>
+      )}
+    </NextUIProvider>
   );
 };
 
