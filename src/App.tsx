22e60b34eb3e47e51dfa8660da82e6ec9578225f
Diego Pinto <diego.pinto357@gmail.com>
2023-05-21 00:01:33 -0300

    Add page content manager

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,60 @@ import ItemsList, { ItemsListProps } from './ItemsList';
 import NodejsLoader from './NodejsLoader';
 import core from './core';
 
+enum ContentPage {
+  Loader,
+  Idle,
+  QrReader,
+  QrResults,
+}
+
 const App = () => {
-  const [renderQrCode, setRenderQrCode] = useState<boolean>(false);
+  const [contentPage, setContentPage] = useState<ContentPage>(ContentPage.Idle);
   const [items, setItems] = useState<ItemsListProps['items']>([]);
 
   const onButtonClick = useCallback(async () => {
-    setRenderQrCode(true);
+    setContentPage(ContentPage.QrReader);
   }, []);
 
   const onQrCodeReaderClose = useCallback(async (data?: string) => {
-    setRenderQrCode(false);
-
     if (data) {
       const key = data.match(/\?p=([^&]*)/)![1];
       const nfData = await core.getNfData(key);
       setItems(nfData.items);
+      setContentPage(ContentPage.QrResults);
+      return;
     }
+
+    setContentPage(ContentPage.Idle);
   }, []);
+
+  const renderContentPage = useCallback(
+    (selectedContentPage: ContentPage) => {
+      switch (selectedContentPage) {
+        case ContentPage.Loader:
+          break;
+
+        case ContentPage.Idle:
+          return <Button onPress={onButtonClick}>Parse NF</Button>;
+
+        case ContentPage.QrReader:
+          return <QrCodeReader onClose={onQrCodeReaderClose} />;
+
+        case ContentPage.QrResults:
+          return <ItemsList items={items} />;
+      }
+
+      return null;
+    },
+    [items, onButtonClick, onQrCodeReaderClose]
+  );
 
   return (
     <NextUIProvider>
-      {renderQrCode ? (
-        <QrCodeReader onClose={onQrCodeReaderClose} />
-      ) : (
-        <Container sm>
-          <NodejsLoader />
-          <br />
-          <Button onPress={onButtonClick}>Parse NF</Button>
-          <br />
-          <ItemsList items={items} />
-        </Container>
-      )}
+      <Container sm>
+        <NodejsLoader />
+        {renderContentPage(contentPage)}
+      </Container>
     </NextUIProvider>
   );
 };
