e1993fafaf494475d56b40b21e1a255a8843372a
Diego Pinto <diego.pinto357@gmail.com>
2023-07-19 22:20:38 -0300

    Adjust storage driver on nodejs to handle data as both string and object

--- src/nodejs/services/storage.js
+++ src/nodejs/services/storage.js
@@ -7,12 +7,16 @@ const userDataFolder = './userData';
 
 const writeFile = async (filename, data) => {
   const fullPath = path.join(userDataFolder, filename);
-  return await fs.writeFile(fullPath, JSON.stringify(data, null, 2), 'utf-8');
+  const fileContent =
+    typeof data === 'string' ? data : JSON.stringify(data, null, 2);
+  return await fs.writeFile(fullPath, fileContent, 'utf-8');
 };
 
 const readFile = async filename => {
   const fullPath = path.join(userDataFolder, filename);
-  return JSON.parse(await fs.readFile(fullPath, 'utf-8'));
+  const fileContent = await fs.readFile(fullPath, 'utf-8');
+  const fileExtension = filename.split('.')[1];
+  return fileExtension === 'json' ? JSON.parse(fileContent) : fileContent;
 };
 
 module.exports = {
