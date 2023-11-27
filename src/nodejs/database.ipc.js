0455e9e24765457f906f7abc1c9f78d4bb547de0
Diego Pinto <diego.pinto357@gmail.com>
2023-11-26 22:21:14 -0300

    Add database methods to ipc sever

--- src/nodejs/database.ipc.js
+++ src/nodejs/database.ipc.js
@@ -4,13 +4,63 @@ const ipc = require('./ipc');
 const database = require('./services/database');
 
 ipc.on(
+  'post:database/find',
+  async ({ databaseName, collectionName, filter, options }, reply) => {
+    const data = await database.find(
+      databaseName,
+      collectionName,
+      filter,
+      options
+    );
+    reply(data);
+  }
+);
+
+ipc.on(
   'post:database/findOne',
   async ({ databaseName, collectionName, filter, options }, reply) => {
-    console.log({ databaseName, collectionName, filter, options });
     const data = await database.findOne(
       databaseName,
       collectionName,
       filter,
+      options
+    );
+    reply(data);
+  }
+);
+
+ipc.on(
+  'post:database/insert',
+  async ({ databaseName, collectionName, documents }, reply) => {
+    const { data } = await database.insert(
+      databaseName,
+      collectionName,
+      documents
+    );
+    reply(data);
+  }
+);
+
+ipc.on(
+  'post:database/insertOne',
+  async ({ databaseName, collectionName, document }, reply) => {
+    const { data } = await database.insertOne(
+      databaseName,
+      collectionName,
+      document
+    );
+    reply(data);
+  }
+);
+
+ipc.on(
+  'post:database/updateOne',
+  async ({ databaseName, collectionName, filter, update, options }, reply) => {
+    const { data } = await database.updateOne(
+      databaseName,
+      collectionName,
+      filter,
+      update,
       options
     );
     reply(data);
