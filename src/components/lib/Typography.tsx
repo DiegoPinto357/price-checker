b0b6bd807a5b11634ab7e26159ad43a9843f316d
Diego Pinto <diego.pinto357@gmail.com>
2024-08-26 18:12:01 -0300

    Fix typography styles

--- src/components/lib/Typography.tsx
+++ src/components/lib/Typography.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from 'tailwind-merge';
+
 import type { PropsWithChildren } from 'react';
 
 type Props = {
@@ -15,28 +17,31 @@ const Typography = ({
   switch (variant) {
     case 'h1':
       return (
-        <h1 className={`mb-4 text-2xl font-medium ${className}`} {...rest}>
+        <h1
+          className={twMerge('mb-4 text-2xl font-medium', className)}
+          {...rest}
+        >
           {children}
         </h1>
       );
 
     case 'h2':
       return (
-        <h2 className={`mb-2 text-xl ${className}`} {...rest}>
+        <h2 className={twMerge('mb-2 text-xl', className)} {...rest}>
           {children}
         </h2>
       );
 
     case 'h3':
       return (
-        <h3 className={`mb-1 text-lg ${className}`} {...rest}>
+        <h3 className={twMerge('mb-1 text-lg', className)} {...rest}>
           {children}
         </h3>
       );
 
     case 'h4':
       return (
-        <h4 className={`mb-0.5 text-md ${className}`} {...rest}>
+        <h4 className={twMerge('mb-0.5 text-md', className)} {...rest}>
           {children}
         </h4>
       );
