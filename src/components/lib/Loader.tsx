dd84539b0ef241ab07b07ddb43d3b2ef777aa479
Diego Pinto <diego.pinto357@gmail.com>
2025-01-24 16:04:55 -0300

    Add fullScreen option to Loader

--- src/components/lib/Loader.tsx
+++ src/components/lib/Loader.tsx
@@ -1,11 +1,22 @@
+import { twMerge } from 'tailwind-merge';
 import { Spinner } from '@nextui-org/react';
 
-const Loader = ({ opaque }: { opaque?: boolean }) => {
-  const classes = opaque ? 'bg-white' : 'bg-[#00000050]';
+type Props = {
+  fullScreen?: boolean;
+  opaque?: boolean;
+};
+
+const Loader = ({ opaque, fullScreen = true }: Props) => {
+  const position = fullScreen ? 'fixed' : 'relative';
+  const backgroundColor = opaque ? 'bg-white' : 'bg-[#00000050]';
 
   return (
     <div
-      className={`fixed w-full h-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 ${classes}`}
+      className={twMerge(
+        'relative w-full h-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50',
+        position,
+        backgroundColor
+      )}
     >
       <Spinner
         className="scale-150 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
