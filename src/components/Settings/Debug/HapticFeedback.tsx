839b2d74511a3fdb024f6550dd0e463f4d07e8b9
Diego Pinto <diego.pinto357@gmail.com>
2025-01-24 11:37:55 -0300

    Add haptics feedback to debug page

---
+++ src/components/Settings/Debug/HapticFeedback.tsx
@@ -0,0 +1,97 @@
+import { Haptics, ImpactStyle, NotificationType } from '@capacitor/haptics';
+import { Button } from '@nextui-org/react';
+import Typography from '../../lib/Typography';
+
+const HapticFeedback = () => {
+  return (
+    <div>
+      <Typography variant="h3">Feedback táctil</Typography>
+
+      <Typography variant="h4">Impact</Typography>
+      <div className="flex justify-between mb-4">
+        <Button
+          color="primary"
+          onPress={async () => {
+            await Haptics.impact({ style: ImpactStyle.Light });
+          }}
+        >
+          Light
+        </Button>
+        <Button
+          color="primary"
+          onPress={async () => {
+            await Haptics.impact({ style: ImpactStyle.Medium });
+          }}
+        >
+          Medium
+        </Button>
+        <Button
+          color="primary"
+          onPress={async () => {
+            await Haptics.impact({ style: ImpactStyle.Heavy });
+          }}
+        >
+          Heavy
+        </Button>
+      </div>
+
+      <Typography variant="h4">Notification</Typography>
+      <div className="flex justify-between mb-4">
+        <Button
+          color="primary"
+          onPress={async () => {
+            await Haptics.notification({ type: NotificationType.Success });
+          }}
+        >
+          Success
+        </Button>
+        <Button
+          color="primary"
+          onPress={async () => {
+            await Haptics.notification({ type: NotificationType.Warning });
+          }}
+        >
+          Warning
+        </Button>
+        <Button
+          color="primary"
+          onPress={async () => {
+            await Haptics.notification({ type: NotificationType.Error });
+          }}
+        >
+          Error
+        </Button>
+      </div>
+
+      <Typography variant="h4">Selection</Typography>
+      <div className="flex justify-between mb-4">
+        <Button
+          color="primary"
+          onPress={async () => {
+            await Haptics.selectionStart();
+          }}
+        >
+          Start
+        </Button>
+        <Button
+          color="primary"
+          onPress={async () => {
+            await Haptics.selectionChanged();
+          }}
+        >
+          Change
+        </Button>
+        <Button
+          color="primary"
+          onPress={async () => {
+            await Haptics.selectionEnd();
+          }}
+        >
+          End
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default HapticFeedback;
--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -3,6 +3,7 @@ import { Button } from '@nextui-org/react';
 import Typography from '../lib/Typography';
 import Loader from '../lib/Loader';
 import dataSync from '../../dataSync';
+import HapticFeedback from './Debug/HapticFeedback';
 
 const Settings = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -17,13 +18,20 @@ const Settings = () => {
     <div data-testid="settings">
       <Typography variant="h2">Debug</Typography>
 
-      <Button
-        className="w-full md:w-1/3"
-        color="primary"
-        onPress={handleSyncButtonClick}
-      >
-        Sincronizar Banco de Dados
-      </Button>
+      <div className="flex flex-col gap-8">
+        <div>
+          <Typography variant="h3">Banco de dados</Typography>
+          <Button
+            className="w-full md:w-1/3"
+            color="primary"
+            onPress={handleSyncButtonClick}
+          >
+            Sincronizar
+          </Button>
+        </div>
+
+        <HapticFeedback />
+      </div>
 
       {isLoading && <Loader />}
     </div>
