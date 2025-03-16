fe3ad9eda3e4e1f2c9195cf77351851e3b2982b9
Diego Pinto <diego.pinto357@gmail.com>
2025-03-16 19:58:50 -0300

    Refactor menu

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,29 +1,21 @@
-import { PropsWithChildren, useState } from 'react';
-import { useDisclosure } from '@nextui-org/react';
+import { PropsWithChildren } from 'react';
+
 import Typography from '../lib/Typography';
 import Menu from './Menu';
-import ContentModal from './ContentModal';
-
-import type { View } from './types';
 
 type Props = PropsWithChildren & {
   title: string;
 };
 
 const MainContent = ({ title, children }: Props) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [view, setView] = useState<View>('settings');
-
   return (
     <div className="flex flex-col justify-between h-full">
       <div className="flex gap-3 mx-4">
-        <Menu onOpen={onOpen} onSetView={setView} />
+        <Menu />
         <Typography variant="h1">{title}</Typography>
       </div>
 
       <div className="overflow-hidden h-full">{children}</div>
-
-      <ContentModal view={view} isOpen={isOpen} onClose={onClose} />
     </div>
   );
 };
--- src/components/MainContent/Menu.tsx
+++ src/components/MainContent/Menu.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Button,
   Dropdown,
@@ -5,37 +6,41 @@ import {
   DropdownMenu,
   DropdownItem,
 } from '@nextui-org/react';
+import { useDisclosure } from '@nextui-org/react';
 import { LuMenu } from 'react-icons/lu';
 import { GoGear } from 'react-icons/go';
+import ContentModal from './ContentModal';
 
 import type { View } from './types';
 
-type Props = {
-  onOpen: () => void;
-  onSetView: (view: View) => void;
-};
+const Menu = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [view, setView] = useState<View>('settings');
 
-const Menu = ({ onOpen, onSetView }: Props) => {
   return (
-    <Dropdown>
-      <DropdownTrigger>
-        <Button aria-label="menu" variant="light" size="sm" isIconOnly>
-          <LuMenu className="w-7 h-7 text-gray-600" />
-        </Button>
-      </DropdownTrigger>
-      <DropdownMenu
-        aria-label="menu actions"
-        variant="bordered"
-        onAction={key => {
-          onSetView(key as View);
-          onOpen();
-        }}
-      >
-        <DropdownItem key="settings" startContent={<GoGear />}>
-          Configurações
-        </DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
+    <>
+      <Dropdown>
+        <DropdownTrigger>
+          <Button aria-label="menu" variant="light" size="sm" isIconOnly>
+            <LuMenu className="w-7 h-7 text-gray-600" />
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu
+          aria-label="menu actions"
+          variant="bordered"
+          onAction={key => {
+            setView(key as View);
+            onOpen();
+          }}
+        >
+          <DropdownItem key="settings" startContent={<GoGear />}>
+            Configurações
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+
+      <ContentModal view={view} isOpen={isOpen} onClose={onClose} />
+    </>
   );
 };
 
