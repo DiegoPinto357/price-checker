acfeda4f9c85b2c308bb1c38fcc05b732575170e
Diego P dos Santos <diego.pinto357@gmail.com>
2025-10-13 14:03:56 -0300

    fix: update content paths in Tailwind configuration for consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 import { nextui } from '@nextui-org/react';
 
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    'index.html',
+    'src/**/*.{js,ts,jsx,tsx}',
+    '!src/nodejs/node_modules',
+    'node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
